ef0ed3e6e867297486584158b5f6ca5bb28986ff
Tobias Videla G <tobiasvidelaug14@gmail.com>
2023-01-23 16:00:42 -0300

    adding slider and animation to principal page

--- logica/animations.js
+++ logica/animations.js
@@ -25,7 +25,6 @@ setInterval(()=>{ //repetir
     desplazarX()
 },2000)
 
-
 // ANIMAR ICONOS DE TITULO
 const iconos = document.querySelectorAll('.iconos')
 
@@ -126,8 +125,76 @@ hobbies.addEventListener('click',()=>{
 })
 //cambiar por promises para cuando (pasado un tiempo de hacer click) se reinicie la animación automática
 
-// ANIMAR FOTO DE PERFIL
+/* ANIMAR FOTO DE PERFIL //descartado temporalmente
+const profile = document.querySelector('#profile')
+function profileFrames () {
+    const floatingImg = profile.animate([
+        {transform: 'translateY(0px)'},
+        {transform: 'translateY(-5px)'},
+        {transform: 'translateY(-10px)'},
+        // {transform: 'translateY(-15px)'},
+        // {transform: 'translateY(-20px)'},
+        // {transform: 'translateY(-15px)'},
+        // {transform: 'translateY(-10px)'},
+        {transform: 'translateY(-5px)'},
+        {transform: 'translateY(0px)'},
+        // {transform: 'translateY(5px)'},
+        // {transform: 'translateY(10px)'},
+        // {transform: 'translateY(15px)'},
+        // {transform: 'translateY(20px)'},
+        // {transform: 'translateY(15px)'},
+        // {transform: 'translateY(10px)'},
+        // {transform: 'translateY(5px)'},
+        // {transform: 'translateY(0px)'},
+    ],{
+        easing: 'ease-in-out',
+        iterations: Infinity,
+        duration: 2000,
+        direction: 'alternate',
+    })
+    return floatingImg.finished
+}
+requestAnimationFrame(profileFrames)*/
 
 // ANIMAR SLIDER
+const slider = document.querySelector('#slider'),
+      btnIzq = document.querySelector('#btn-izq'),
+      btnDer = document.querySelector('#btn-der')
+let sliderSection = document.querySelectorAll('.slider-section'),
+    sliderSectionLast = sliderSection[sliderSection.length - 1]
+      
+slider.insertAdjacentElement('afterbegin', sliderSectionLast) //desplazar el último elemento al inicio
+
+function moverIzq(){ //funcion para desplazar hacia la izquierda
+    let sliderSection = document.querySelectorAll('.slider-section'),
+        sliderSectionLast = sliderSection[sliderSection.length - 1]
+    slider.style.marginLeft = '0%'
+    slider.style.transition = 'all 1.2s'
+    setTimeout(() => {
+        slider.style.transition = 'none'   
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast)
+        slider.style.marginLeft = '-100%'
+    },1200);
+}
+function moverDer () { //funcion para desplazar hacia la derecha
+    let sliderSectionFirst = document.querySelectorAll('.slider-section')[0]
+    slider.style.marginLeft = '-200%'
+    slider.style.transition = 'all 1.2s'
+    setTimeout(() => {
+        slider.style.transition = 'none'   
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst)
+        slider.style.marginLeft = '-100%'
+    },1200);
+}
+btnDer.addEventListener('click',()=>{
+    moverDer()
+})
+btnIzq.addEventListener('click',()=>{
+    moverIzq()
+})
+setInterval(() => { //repetir
+    moverDer()
+}, 15000);
+
 // ANIMAR ICONOS DE CONTACTO
 // ANIMAR TRAJECTOR

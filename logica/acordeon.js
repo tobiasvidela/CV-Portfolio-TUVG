827b7dc7d87d11c9d1cc35779a7f5a0f156e3633
Tobias Videla G <tobiasvidelaug14@gmail.com>
2023-01-24 09:43:47 -0300

    fixed animations on Trajecotry Page

--- logica/acordeon.js
+++ logica/acordeon.js
@@ -1,40 +1,57 @@
 'use strict'
 // ANIMAR TRAJECTORY
 const columna = document.querySelectorAll('.columna')
+let clicks = 0, timer = null //agregar esto y el demás código relacionado arregló bugs de display luego de animaciones
+//Sí, pude haber usado funciones asíncronas pero esta solución me pareció menos laboriosa
 
 columna.forEach(item =>{
     let descripcion = item.children[1],
         badges = item.children[2]
     item.addEventListener('click',()=>{ //al hacer click, animar
-        descripcion.classList.toggle('mostrar-descripción')
-        setTimeout(() => { //retardar badges
-            badges.classList.toggle('mostrar-descripción')
-        }, 500);
-        if (descripcion.classList.contains('mostrar-descripción')) {
-            descripcion.style.height = `${descripcion.scrollHeight}px`
-            //scrollHeight devuelve la altura de un elemento incluyendo el padding pero no el scrollbar, ni el margin ni borders
-        } else {
-            descripcion.style.height = '0px' // esconder
+        clicks++ // cuento los cliks
+        if (clicks === 1) {
+            timer = setTimeout(() => {
+                
+                descripcion.classList.toggle('mostrar-descripción')
+                setTimeout(() => { //retardar badges
+                    badges.classList.toggle('mostrar-descripción')
+                }, 300);
+                if (descripcion.classList.contains('mostrar-descripción')) {
+                    descripcion.style.height = `${descripcion.scrollHeight}px`
+                    //scrollHeight devuelve la altura de un elemento incluyendo el padding pero no el scrollbar, ni el margin ni borders
+                } else {
+                    descripcion.style.height = '0px' // esconder
+                }
+                //Mostrar luego de esconder la descripción
+                //me gustó más porque genera un efecto rebote
+                if (badges.classList.contains('mostrar-descripción')) {
+                    setTimeout(() => {
+                        badges.style.height = `${badges.scrollHeight}px`
+                    }, 500);
+                } else {
+                    badges.style.height = '0px'
+                }
+
+                clicks = 0 // resetear clicks
+                
+                //Animar después de (y esconder junto con) la descripción
+                // if (badges.classList.contains('mostrar-descripción')) {
+                //     badges.style.height = '0px'
+                // } else {
+                //     setTimeout(() => { // animar luego de esconder el anterior
+                //         badges.style.height = `${badges.scrollHeight}px`
+                //     }, 800);
+                // }
+
+
+            }, 400); //400 milisegundos para que la animación sea fluida y se puedan detectar bien los dobleclicks
+        } else { //si hay muchos clicksseguidos 
+            clearTimeout(timer)
+            alert('Please, be more gentle.')
+            clicks = 0
         }
-        //Mostrar luego de esconder la descripción
-        //me gustó más porque genera un efecto rebote
-        if (badges.classList.contains('mostrar-descripción')) {
-            setTimeout(() => {
-                badges.style.height = `${badges.scrollHeight}px`
-            }, 800);
-            /*hay un bug cuando se clickea demasiado, debería usar un función asíncrona 
-            para esperar que termine uno para animar el otro. Pero no me queda mucho tiempo 
-            para seguir probando y probando cosas, luego lo mejoraré. (estoy haciendo cursos y trbaajando)*/
-        } else {
-            badges.style.height = '0px'
-        }
-        //Animar después de (y esconder junto con) la descripción
-        // if (badges.classList.contains('mostrar-descripción')) {
-        //     badges.style.height = '0px'
-        // } else {
-        //     setTimeout(() => { // animar luego de esconder el anterior
-        //         badges.style.height = `${badges.scrollHeight}px`
-        //     }, 800);
-        // }
+    })
+    item.addEventListener('dblclick',e=>{
+        e.preventDefault() //evitar doble click
     })
 })

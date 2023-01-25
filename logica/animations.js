d5224ea814bbca2bb80b7373dd60a0f0b701414b
Tobias Videla G <tobiasvidelaug14@gmail.com>
2023-01-25 19:47:38 -0300

    improved redirection function for Projects

--- logica/animations.js
+++ logica/animations.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 // ANIMAR COMPUTADORA
-const skillIcons = document.querySelector('.skillIcons') //obtener elemento
+const skillIcons = document.querySelector('.skillIcons'); //obtener elemento
 
 function trasladarX (elementos) { //animación para desplazar horizontalmente
     const animation = elementos.animate([
@@ -13,35 +13,36 @@ function trasladarX (elementos) { //animación para desplazar horizontalmente
         easing: 'ease-in-out',
         iterations: 1,
         duration: 900
-    })
-    return animation.finished
-}
+    });
+    return animation.finished;
+};
 function intercambiar (elemento,direccion = null) { //intercambiar imagenes dentro de un elemento contenedor
-    if (typeof direccion != 'string') return console.error('direction must be a string value')
+    //verificar que la dirección ingresada sea un string
+    if (typeof direccion != 'string') {
+        return console.error('direction must be a string value');
+    };
     // tranformar dirección a minúscula
-    let sentido = direccion.toLowerCase()
+    let sentido = direccion.toLowerCase();
     //ejecutar la animación
     if (sentido == 'x') { //para sentido horizontal
-        trasladarX(elemento)
-        .then(()=>{ //luego de la animación
+        trasladarX(elemento).then(()=>{ //luego de la animación
             //posicionar al último la primera imagen del elemento
-            elemento.appendChild(elemento.querySelectorAll(`.${elemento.className} > img`)[0])
-        })
+            elemento.appendChild(elemento.querySelectorAll(`.${elemento.className} > img`)[0]);
+        });
     } else if (sentido == 'y') { //para sentido vertical
-        trasladarY(elemento)
-        .then(()=>{
-            elemento.appendChild(elemento.querySelectorAll(`.${elemento.className} > img`)[0])
-        })
+        trasladarY(elemento).then(()=>{
+            elemento.appendChild(elemento.querySelectorAll(`.${elemento.className} > img`)[0]);
+        });
     } else {
-        console.error('any direction is selected') //ninguna dirección fue pasada como parámetro
-    }
-}
+        console.error('any direction is selected'); //ninguna dirección fue pasada como parámetro
+    };
+};
 setInterval(()=>{ //repetir
-    intercambiar(skillIcons,'x')
-},1900)// 1 segundo más que la animación (espera un segundo antes de repetir la animación)
+    intercambiar(skillIcons,'x');
+},1900); // 1 segundo más que la animación (espera un segundo antes de repetir la animación)
 
 // ANIMAR ICONOS DE TITULO
-const iconos = document.querySelectorAll('.iconos') //obtener íconos (hero)
+const iconos = document.querySelectorAll('.iconos'); //obtener íconos (hero)
 
 // animaciones de iconos:
 function sacudir (icon) { // animación de sacudida
@@ -61,9 +62,9 @@ function sacudir (icon) { // animación de sacudida
         easing: 'ease-out',
         iterations: 1,
         duration: 700,
-    })
-    return sacudida.finished
-}
+    });
+    return sacudida.finished;
+};
 function sacudirYLatido (icon) { // animación de sacudir y latido
     const shakeAndBeat = icon.animate([
         {transform: 'TranslateX(-3%)'},
@@ -84,9 +85,9 @@ function sacudirYLatido (icon) { // animación de sacudir y latido
         easing: 'ease',
         iterations: 1,
         duration: 1500
-    })
-    return shakeAndBeat.finished
-}
+    });
+    return shakeAndBeat.finished;
+};
 function girarYVoltear (icon) { // animación de girar y voltear
     const turnAndFlip = icon.animate([
         //Voltear
@@ -101,22 +102,22 @@ function girarYVoltear (icon) { // animación de girar y voltear
         easing: 'ease',
         iterations: 1,
         duration: 1500,
-    })
-    return turnAndFlip.finished
-}
+    });
+    return turnAndFlip.finished;
+};
 // asignar animaciones a los íconos
 iconos[0].addEventListener('mouseover',()=>{ //fa-circle-question
-    sacudir(iconos[0])
-})
+    sacudir(iconos[0]);
+});
 iconos[1].addEventListener('mouseover',()=>{ //fa-heart
-    sacudirYLatido(iconos[1])
-})
+    sacudirYLatido(iconos[1]);
+});
 iconos[2].addEventListener('mouseover',()=>{ //fa-user-tie
-    girarYVoltear(iconos[2])
-})
+    girarYVoltear(iconos[2]);
+});
 
 // ANIMAR HOBBIES Y GUSTOS
-const hobbies = document.querySelector('.hobbies')
+const hobbies = document.querySelector('.hobbies');
 
 function trasladarY (elementos) { // animación para desplazar verticalmente
     const animation = elementos.animate([
@@ -127,54 +128,77 @@ function trasladarY (elementos) { // animación para desplazar verticalmente
         easing: 'linear',
         iterations: 1,
         duration: 700,
-    })
-    return animation.finished
-}
+    });
+    return animation.finished;
+};
 let animarHobbies = setInterval(() => {
-    intercambiar(hobbies,'y')
-}, 4000)
-hobbies.addEventListener('click',()=>{
-    intercambiar(hobbies,'y')
-    clearInterval(animarHobbies)
-})
+    intercambiar(hobbies,'y');
+}, 4000);
+let clicks = 0, timer = null; //prevención de clicks
+hobbies.addEventListener('click',()=>{ //animar manualmente
+    //prevenir que se hagan demasiados clicks seguidos
+    clicks++;
+    if (clicks === 1) {
+        timer = setTimeout(() => {
+            intercambiar(hobbies,'y');
+            clearInterval(animarHobbies);
+            clicks = 0;
+        }, 750);
+    } else {
+        clearTimeout(timer);
+        swal({
+            title: 'STOP!',
+            text: 'Please, be more patient.\nDon\'t treat this page like that.',
+            icon: 'warning',
+            button: false,
+            timer: 3200,
+        });
+        clicks = 0;
+    };
+    hobbies.addEventListener('dblclick',e=>{
+        e.preventDefault();
+    });
+    /* revisar acordeon.js para entender mejor lo de los clicks */
+});
 
 // ANIMAR SLIDER
 const slider = document.querySelector('#slider'),
       btnIzq = document.querySelector('#btn-izq'),
-      btnDer = document.querySelector('#btn-der')
+      btnDer = document.querySelector('#btn-der');
 let sliderSection = document.querySelectorAll('.slider-section'),
-    sliderSectionLast = sliderSection[sliderSection.length - 1]
+    sliderSectionLast = sliderSection[sliderSection.length - 1];
       
-slider.insertAdjacentElement('afterbegin', sliderSectionLast) //desplazar el último elemento al inicio
+slider.insertAdjacentElement('afterbegin', sliderSectionLast); //desplazar el último elemento al inicio
 
 function moverIzq(){ //funcion para desplazar hacia la izquierda
     let sliderSection = document.querySelectorAll('.slider-section'),
-        sliderSectionLast = sliderSection[sliderSection.length - 1]
-    slider.style.marginLeft = '0%'
-    slider.style.transition = 'all 1.2s'
+        sliderSectionLast = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = '0%';
+    slider.style.transition = 'all 1.2s';
     setTimeout(() => {
-        slider.style.transition = 'none'   
-        slider.insertAdjacentElement('afterbegin',sliderSectionLast)
-        slider.style.marginLeft = '-100%'
+        slider.style.transition = 'none'; 
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+        slider.style.marginLeft = '-100%';
     },1200);
-}
+};
 function moverDer () { //funcion para desplazar hacia la derecha
-    let sliderSectionFirst = document.querySelectorAll('.slider-section')[0]
-    slider.style.marginLeft = '-200%'
-    slider.style.transition = 'all 1.2s'
+    let sliderSectionFirst = document.querySelectorAll('.slider-section')[0];
+    slider.style.marginLeft = '-200%';
+    slider.style.transition = 'all 1.2s';
     setTimeout(() => {
-        slider.style.transition = 'none'   
-        slider.insertAdjacentElement('beforeend',sliderSectionFirst)
-        slider.style.marginLeft = '-100%'
+        slider.style.transition = 'none';
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst);
+        slider.style.marginLeft = '-100%';
     },1200);
-}
+};
 btnDer.addEventListener('click',()=>{
-    moverDer()
-})
+    moverDer();
+});
 btnIzq.addEventListener('click',()=>{
-    moverIzq()
-})
+    moverIzq();
+});
 setInterval(() => { //repetir slider
-    moverDer()
+    moverDer();
 }, 15000);
 
+/* Espero tener el código casi todo en inglés mayormente y comentarios en español mayormente ayude a el/los evaluadores a entender mejor el código... */
--- logica/redirection.js
+++ logica/redirection.js
@@ -1,70 +1,99 @@
-'use strict'
-const proyectosGH = document.querySelectorAll('.proyectosGH'),
-      proyectos = document.querySelectorAll('.proyectos')
+'use strict';
+const iconosContenedor = document.querySelectorAll('.iconos-contenedor > a');
 
-proyectosGH[0].addEventListener('click', ()=>{ //redirigir a GitHub de CrochetMind
-    swal({
-        title: 'Wait!',
-        text: `You're being redirected to the repository of CrochetMind.\n\nSee you soon!`,
-        buttons: false,
-        timer: 2500,
-    })
-    setTimeout(() => {
-        window.open('https://github.com/tobiasvidela/CrochetMind','_blank','noopener noreferrer') //abrir de manera segura
-    }, 2500);
-})
-proyectos[0].addEventListener('click', ()=>{ //redirigir a CrochetMind
-    swal({
-        title: 'Wait!',
-        text: `You're being redirected to CrochetMind.\n\nSee you soon!`,
-        buttons: false,
-        timer: 2500,
-    })
-    setTimeout(() => {
-        window.open('https://tobiasvidela.github.io/CrochetMind/','_blank','noopener noreferrer')
-    }, 3000);
-})
-proyectosGH[1].addEventListener('click', ()=>{ //redirigir a GitHub de CalcWeekDay
-    swal({
-        title: 'Wait!',
-        text: `You're being redirected to the repository of CalcWeekDay.\n\nSee you soon!`,
-        buttons: false,
-        timer: 2500,
-    })
-    setTimeout(() => {
-        window.open('https://github.com/tobiasvidela/CalcWeekDay','_blank','noopener noreferrer')
-    }, 2500);
-})
-proyectos[1].addEventListener('click', ()=>{ //redirigir a CalcWeekDay
-    swal({
-        title: 'Wait!',
-        text: `You're being redirected to CalcWeekDay.\n\nSee you soon!`,
-        buttons: false,
-        timer: 2500,
-    })
-    setTimeout(() => {
-        window.open('https://tobiasvidela.github.io/CalcWeekDay/','_blank','noopener noreferrer')
-    }, 3000);
-})
-proyectosGH[2].addEventListener('click', ()=>{ //redirigir a GitHub de SpeedCubeVM
-    swal({
-        title: 'Wait!',
-        text: `You're being redirected to the repository of SpeedCubeVM.\n\nSee you soon!`,
-        buttons: false,
-        timer: 2500,
-    })
-    setTimeout(() => {
-        window.open('https://github.com/tobiasvidela/SpeedCubeVM','_blank','noopener noreferrer')
-    }, 2500);
-})
-proyectos[2].addEventListener('click', ()=>{ //redirigir a SpeedCubeVM
-    swal({
-        title: 'Wait!',
-        text: `You're being redirected to SpeedCubeVM.\n\nSee you soon!`,
-        buttons: false,
-        timer: 2500,
-    })
-    setTimeout(() => {
-        window.open('https://tobiasvidela.github.io/SpeedCubeVM/','_blank','noopener noreferrer')
-    }, 3000);
-})
+iconosContenedor.forEach(icon => {
+    if (icon.className == 'proyectosGH') {
+        icon.addEventListener('click', () => { // redirigir al GitHub del proyecto seleccionado
+            swal({
+                title: 'Wait!',
+                text: `You're being redirected to the repository of ${icon.name}.\n\nSee you soon!`,
+                buttons: false,
+                timer: 2500,
+            });
+            setTimeout(() => {
+                window.open(`https://github.com/tobiasvidela/${icon.name}`, '_blank', 'noopener noreferrer'); // abrir de manera segura
+            }, 2500);
+        });
+    } else if (icon.className == 'proyectos') { // redirigir al proyecto seleccionado
+        icon.addEventListener('click', () => {
+            swal({
+                title: 'Wait!',
+                text: `You're being redirected to ${icon.name}.\n\nSee you soon!`,
+                buttons: false,
+                timer: 2500,
+            });
+            setTimeout(() => {
+                window.open(`https://tobiasvidela.github.io/${icon.name}`, '_blank', 'noopener noreferrer'); // abrir de manera segura
+            }, 2500);
+        });
+    } else {
+        console.error('There was an error while selecting the project redirection.');
+    };
+});
+
+// proyectosGH[0].addEventListener('click', ()=>{ //redirigir a GitHub de CrochetMind
+//     swal({
+//         title: 'Wait!',
+//         text: `You're being redirected to the repository of CrochetMind.\n\nSee you soon!`,
+//         buttons: false,
+//         timer: 2500,
+//     });
+//     setTimeout(() => {
+//         window.open('https://github.com/tobiasvidela/CrochetMind','_blank','noopener noreferrer'); //abrir de manera segura
+//     }, 2500);
+// });
+// proyectos[0].addEventListener('click', ()=>{ //redirigir a CrochetMind
+//     swal({
+//         title: 'Wait!',
+//         text: `You're being redirected to CrochetMind.\n\nSee you soon!`,
+//         buttons: false,
+//         timer: 2500,
+//     });
+//     setTimeout(() => {
+//         window.open('https://tobiasvidela.github.io/CrochetMind/','_blank','noopener noreferrer');
+//     }, 3000);
+// });
+// proyectosGH[1].addEventListener('click', ()=>{ //redirigir a GitHub de CalcWeekDay
+//     swal({
+//         title: 'Wait!',
+//         text: `You're being redirected to the repository of CalcWeekDay.\n\nSee you soon!`,
+//         buttons: false,
+//         timer: 2500,
+//     });
+//     setTimeout(() => {
+//         window.open('https://github.com/tobiasvidela/CalcWeekDay','_blank','noopener noreferrer');
+//     }, 2500);
+// });
+// proyectos[1].addEventListener('click', ()=>{ //redirigir a CalcWeekDay
+//     swal({
+//         title: 'Wait!',
+//         text: `You're being redirected to CalcWeekDay.\n\nSee you soon!`,
+//         buttons: false,
+//         timer: 2500,
+//     });
+//     setTimeout(() => {
+//         window.open('https://tobiasvidela.github.io/CalcWeekDay/','_blank','noopener noreferrer');
+//     }, 3000);
+// });
+// proyectosGH[2].addEventListener('click', ()=>{ //redirigir a GitHub de SpeedCubeVM
+//     swal({
+//         title: 'Wait!',
+//         text: `You're being redirected to the repository of SpeedCubeVM.\n\nSee you soon!`,
+//         buttons: false,
+//         timer: 2500,
+//     });
+//     setTimeout(() => {
+//         window.open('https://github.com/tobiasvidela/SpeedCubeVM','_blank','noopener noreferrer');
+//     }, 2500);
+// });
+// proyectos[2].addEventListener('click', ()=>{ //redirigir a SpeedCubeVM
+//     swal({
+//         title: 'Wait!',
+//         text: `You're being redirected to SpeedCubeVM.\n\nSee you soon!`,
+//         buttons: false,
+//         timer: 2500,
+//     });
+//     setTimeout(() => {
+//         window.open('https://tobiasvidela.github.io/SpeedCubeVM/','_blank','noopener noreferrer');
+//     }, 3000);
+// });
